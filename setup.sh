6907be76da3bff852f2c6167b1a8da307327f162
Jeroen Delcour <jeroendelcour@gmail.com>
2018-08-15 22:43:02 +0200

    alias c=clear, cleanup old duplicate file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # set up symlinks
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -sT ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
