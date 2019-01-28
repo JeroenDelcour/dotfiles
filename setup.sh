e81a6396af2c9d8b9145b131188370d9acc4397a
Jeroen Delcour <jeroendelcour@gmail.com>
2019-01-28 10:36:07 +0100

    setup for zsh with powerline

--- setup.sh
+++ setup.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# set up symlinks
-ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -sT ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/tmux ~/.tmux
+BASEDIR=$(dirname $0)
 
-cd ~/dotfiles/vim/bundle/YouCompleteMe
-python3 install.py
+# set up symlinks
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+ln -sT ${BASEDIR}/vim ~/.vim
+ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
+ln -s ${BASEDIR}/tmux ~/.tmux
+ln -s ${BASEDIR}/zshrc ~/.zshrc
+ln -s ${BASEDIR}/oh-my-zsh ~/.oh-my-zsh
+
+# install zsh and oh-my-zsh
+apt install zsh
+chsh -s `which zsh` # set zsh as default shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # install oh-my-zsh
+
+# setup powerline
+apt install powerline fonts-powerline
+
+# setup virtualenvwrapper
+pip3 install virtualenvwrapper -y
+# stuff to show activate env in zsh
+cp virtualenvs/postactivate ~/.virtualenvs/postactivate
+cp virtualenvs/postdeactivate ~/.virtualenvs/postdeactivate
 
 echo "Setup completed."
